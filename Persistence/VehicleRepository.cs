12d329e9439f3e267ce62f38faa0ec87a63dec93
pmk316 <muralidharkini@yahoo.com>
2017-11-21 19:50:26 +0530

    Adding additional files

---
+++ Persistence/VehicleRepository.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using vega.Core;
+using vega.Core.Models;
+using vega.Extensions;
+using vega.Models;
+
+namespace vega.Persistence
+{
+    public class VehicleRepository : IVehicleRepository
+    {
+        private readonly VegaDbContext context;
+        public VehicleRepository(VegaDbContext context)
+        {
+            this.context = context;
+
+        }
+
+        public async Task<Vehicle> GetVehicle(int id, bool includeRelated = true)
+        {
+            if(!includeRelated)
+                return await context.Vehicles.FindAsync(id);
+
+            return await context.Vehicles
+                .Include(v => v.Features)
+                .ThenInclude(vf => vf.Feature)
+                .Include(v => v.Model)
+                .ThenInclude(m => m.Make)
+                .SingleOrDefaultAsync(v => v.Id == id);
+        }
+
+
+        public async Task<QueryResult<Vehicle>> GetVehicles(VehicleQuery queryObj)
+        {
+            var result = new QueryResult<Vehicle>();
+
+            var query = context.Vehicles
+                .Include(v => v.Model)
+                    .ThenInclude(m => m.Make)
+                .Include(v => v.Features)
+                    .ThenInclude(vf => vf.Feature)
+                    .AsQueryable();                
+
+            //Filtering
+            if (queryObj.MakeId.HasValue)
+                query = query.Where(v => v.Model.MakeId == queryObj.MakeId.Value);
+            
+            if (queryObj.ModelId.HasValue)
+                query = query.Where(v => v.Model.MakeId == queryObj.ModelId.Value);
+
+            //sorting
+            var columnsMap = new Dictionary<string, Expression<Func<Vehicle, Object>>>()
+            {
+                ["make"] = v => v.Model.Make.Name,
+                ["model"] = v => v.Model.Name,
+                ["contactName"] = v => v.ContactName,
+            };
+
+            query = query.ApplyOrdering(queryObj, columnsMap);
+
+            result.TotalItems = await query.CountAsync();
+
+            query = query.ApplyPaging(queryObj);            
+
+            result.Items = await query.ToListAsync();
+
+            return result;
+        }
+
+        public void Add(Vehicle vehicle)
+        {
+            context.Vehicles.Add(vehicle);
+        }
+
+        public void Remove(Vehicle vehicle)
+        {
+            context.Remove(vehicle);
+        }
+
+        
+    }
+}
